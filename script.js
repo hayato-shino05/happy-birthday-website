58fc7b9e940a8af94e772d0f18c141cfd3e2379a
Hayato_shino05 <nthdungvn@gmail.com>
2025-02-17 03:24:14 +0000

    ficxx

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 // Set your birthday here (month is 0-indexed: 0 = January, 11 = December)
 const birthdayMonth = 1;  // Tháng 1
-const birthdayDay = 16;   // Ngày 15
+const birthdayDay = 17;   // Ngày 15
 
 function updateCountdown() {
     const now = new Date();
