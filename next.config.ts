a8d98c140372e26852ed47ff6e67b1b94bc4764c
hayato-shino05 <hayatoshino05@gmail.com>
2026-08-20 13:11:36 +0700

    style(layout): モバイル表示における言語・テーマ配置の縦並び化と設定更新

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,9 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* 追加の設定オプションを記述する場所 */
+  /* 追加の設定オプション */
   reactCompiler: true,
+  allowedDevOrigins: ['localhost:3000', '192.168.1.7', '192.168.1.7:3000'],
 };
 
 export default nextConfig;
